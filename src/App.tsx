7ddb111859cdfc1da2117fedb8ecfb8a53f63ed4
rem44 <79606312+rem44@users.noreply.github.com>
2025-05-15 20:27:36 -0400

    Implement PDF Analyzer with Resizable Panels

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,25 @@ import ResultsViewer from './pages/ResultsViewer';
 import Documents from './pages/Documents';
 import History from './pages/History';
 import Settings from './pages/Settings';
+import { AuthProvider } from './utils/AuthContext';
 import './App.css';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/search" element={<SearchConfig />} />
-          <Route path="/results" element={<ResultsViewer />} />
-          <Route path="/documents" element={<Documents />} />
-          <Route path="/history" element={<History />} />
-          <Route path="/settings" element={<Settings />} />
-        </Routes>
-      </Layout>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/search" element={<SearchConfig />} />
+            <Route path="/results" element={<ResultsViewer />} />
+            <Route path="/documents" element={<Documents />} />
+            <Route path="/history" element={<History />} />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </AuthProvider>
   );
 };
 
